e1dfe1a0a7d32308884b4d419131778fa608e2e6
asocialec <pandolo3@gmail.com>
2016-01-28 13:34:38 +0100

    JS in one external file
    Local data only.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ==========================================================================================
+// Get JSON from API
 function getData(){
 	$.ajax({
 	  	type: 'GET',
@@ -11,22 +13,19 @@ function getData(){
 
   		},
   		error: function(){
-  			alert("Napaka pri prikazovanju podatkov!");
+              showTable(); // for testing only
+  			// alert("Napaka pri prikazovanju podatkov!");
   		}
 	});
 	
 }
 
+// ==========================================================================================
+// Show table
 function showTable (data) {
+    // just for testing
+    data = {"locations":[{"parkingID":33,"latitude":46.0500278,"longitude":14.488073,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 12:35:09.0"},{"parkingID":34,"latitude":46.0500301,"longitude":14.488074,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 12:39:36.0"},{"parkingID":35,"latitude":46.0500213,"longitude":14.4879432,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:02:29.0"},{"parkingID":36,"latitude":46.0500181,"longitude":14.4880478,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:37:00.0"},{"parkingID":37,"latitude":46.0500173,"longitude":14.4880554,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:37:12.0"},{"parkingID":38,"latitude":46.0500523,"longitude":14.4879745,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 17:02:42.0"},{"parkingID":39,"latitude":46.0500475,"longitude":14.4881284,"address":"&Scaron;krab&#x10d;eva ulica 19a, 1000 Ljubljana","startParking":"2016-01-18 17:56:21.0"}]};
 	var items = [];
-	/*data = [
-{"parkingID":33,"latitude":46.0530278,"longitude":14.488073,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 12:35:09"},
-{"parkingID":34,"latitude":46.0500301,"longitude":14.508074,"address":"Jamova ulica 10, 1000 Ljubljana","startParking":"2016-01-18 12:39:36"},
-{"parkingID":35,"latitude":46.3800213,"longitude":14.4879432,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:02:29"},
-{"parkingID":36,"latitude":46.0350181,"longitude":14.4580478,"address":"Dunajska 65, 1000 Ljubljana","startParking":"2016-01-18 13:37:00"},
-{"parkingID":37,"latitude":46.0500173,"longitude":14.4880554,"address":"Ločnikarjeva 1, 1000 Ljubljana","startParking":"2016-01-18 13:37:12"},
-{"parkingID":38,"latitude":46.0800523,"longitude":14.4879745,"address":"Pod brezami 23, 1000 Ljubljana","startParking":"2016-01-18 17:02:42"},
-{"parkingID":39,"latitude":46.0500475,"longitude":14.5581284,"address":"Bobenčkova, 1000 Ljubljana","startParking":"2016-01-18 17:56:21"}];*/
 	$.each(data.locations, function (i, item) {
 		items.push("<tr><td>" + item.address + "</td>" + 
 			"<td>" + item.startParking + "</td>" + 
@@ -40,18 +39,102 @@ function showTable (data) {
 	//console.log(data);
 }
 
+// ==========================================================================================
+// Show google maps inside iFrame
 
-function izbrisi(id){
-	$.ajax({
-  type: 'DELETE',
-  url: 'http://localhost:8080/ParkirajWeb/web/V1_web/parkingID='+id,
-  success: function() {
-    getData();
-  }
-});
+function showMapsIFrame() {
+      // TO-DO: GET JSON from somewhere (ajax)
+  // http://rest.learncode.academy/api/jernejm/lokacije
 
+   locations = {"locations":[{"parkingID":33,"latitude":46.0500278,"longitude":14.488073,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 12:35:09.0"},{"parkingID":34,"latitude":46.0500301,"longitude":14.488074,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 12:39:36.0"},{"parkingID":35,"latitude":46.0500213,"longitude":14.4879432,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:02:29.0"},{"parkingID":36,"latitude":46.0500181,"longitude":14.4880478,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:37:00.0"},{"parkingID":37,"latitude":46.0500173,"longitude":14.4880554,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 13:37:12.0"},{"parkingID":38,"latitude":46.0500523,"longitude":14.4879745,"address":"&Scaron;krab&#x10d;eva ulica 21a, 1000 Ljubljana","startParking":"2016-01-18 17:02:42.0"},{"parkingID":39,"latitude":46.0500475,"longitude":14.4881284,"address":"&Scaron;krab&#x10d;eva ulica 19a, 1000 Ljubljana","startParking":"2016-01-18 17:56:21.0"}]};
+
+   var locations;
+   var items = [];
+
+  $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/ParkirajWeb/web/V1_web',
+      success: function(data) {
+
+        locations = JSON.parse(data);
+        $.each(locations.locations, function(key, item){
+            //console.log(item.address);
+            items.push(item.latitude, item.longitude, item.address);
+          });
+        drawMap(items);
+      },
+      error: function(){
+          console.log("Error. Go to drawmap().");
+          console.log(locations.locations);
+          $.each(locations.locations, function(key, item){
+              //console.log(item.address);
+              items.push(item.latitude, item.longitude, item.address);
+              });
+            drawMap(items);
+        //   alert("Napaka pri prikazovanju podatkov!");
+      }
+  });
+
+function drawMap(items){
+
+    console.log(items);
+    console.log(items[0], items[1], items[2]);
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12, // TO-DO: auto zoom using latlngbound.center
+      center: new google.maps.LatLng(items[0], items[1]),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    // gMaps bounds
+    var latlngbounds = new google.maps.LatLngBounds();
+    // Markers
+    var marker, i;
+    for (i = 0; i < items.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(items[i], items[i+1]),
+        map: map
+      })
+      
+      // bounds
+      {
+        // latlngbounds.extend(new google.maps.latlng(locations[i].latitude, locations[i].longitude));
+      }
+
+      ;
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(items[i+2]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+      i++;
+    }
+}
 }
 
+// ==========================================================================================
+// Delete item
+function izbrisi(id){
+	$.ajax({
+        type: 'DELETE',
+        url: 'http://localhost:8080/ParkirajWeb/web/V1_web/parkingID='+id,
+        success: function() {
+            getData();
+            }
+        });
+    }
+
+
+// ==========================================================================================
+// Edit item
+// TODO:
+    // Open new google maps instance in bootstrap modal.
+    // Enable marker editing
+    // Reverse geocoder to get address from marker position
 function uredi(id){
 
 	alert(id);
